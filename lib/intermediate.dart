0fc771415bbe5bf272d1e21670c6cc82b3ef4c74
wubetayalew <wubetayalewcourse@gmail.com>
2023-12-27 16:35:28 +0300

    complete responsive structre and created constants , theme

---
+++ lib/intermediate.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ssen_company/screens/desktop_responsive.dart';
+import 'package:ssen_company/utils/constants.dart';
+
+import 'screens/state pages/main_page.dart';
+
+class Intermediate extends StatefulWidget {
+  const Intermediate({super.key});
+  static const route = "intermediate";
+
+  @override
+  State<Intermediate> createState() => _IntermediateState();
+}
+
+class _IntermediateState extends State<Intermediate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: (MediaQuery.of(context).size.width > phoneSize)
+            ? const DesktopResponsive()
+            : const MainPage());
+  }
+}
